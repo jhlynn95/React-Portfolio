984364504ce0d765357cac7a4f044530ffc75790
jhlynn95 <109389786+jhlynn95@users.noreply.github.com>
2023-05-22 13:56:55 -0700

    bug fix

---
+++ assets/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import PortfolioContainer from "./components/PortfolioContainer";
+
+// Router 
+import { BrowserRouter as Router } from "react-router-dom";
+
+import "./styles/main.css";
+
+function App() {
+  return (
+
+    <Router>
+
+      <PortfolioContainer />
+
+    </Router>
+
+  );
+  
+}
+
+export default App;
